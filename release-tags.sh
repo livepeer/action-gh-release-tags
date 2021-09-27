8857bf7d684854a858929f911adb11b81f3945d5
Victor Elias <victorgelias@gmail.com>
2021-09-27 19:12:27 -0300

    remove duplicate function wtf

--- release-tags.sh
+++ release-tags.sh
@@ -20,22 +20,6 @@ function getBranches() {
   done
 }
 
-function getBranches() {
-  GH_REF=${GITHUB_HEAD_REF:-$GITHUB_REF}
-  if [[ $GH_REF == refs/heads/* ]]; then
-    GH_REF=${GH_REF#refs/heads/}
-  else
-    GH_REF=$(git branch -a --points-at HEAD \
-        | sed -e 's/^[\* ]*//' -e 's/^remotes\/origin\///' -e '/HEAD /d' \
-        | sort | uniq)
-  fi
-
-  for branch in $GH_REF; do
-    printf "$branch" | sed 's/\//-/g' | tr -cd '[:alnum:]_-'
-    printf " "
-  done
-}
-
 function getTags() {
   PREFIX=$1
   VERSION=$2
